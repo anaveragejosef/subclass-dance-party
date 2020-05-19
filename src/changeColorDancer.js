68d552228767c2540030002a24ee5e0c3695d0c3
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-18 20:44:34 -0700

    Added Notes to changeColorDancer

--- src/changeColorDancer.js
+++ src/changeColorDancer.js
@@ -10,8 +10,10 @@ changeColorDancer.prototype.step = function() {
   // Call the old version (makeDancer) of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
 
+  // Create random RGB numbers by multiplying 255 by Math.random()
   var randomR = Math.floor(Math.random() * 255);
   var randomG = Math.floor(Math.random() * 255);
   var randomB = Math.floor(Math.random() * 255);
+  // Update the border-color wiht our new color combination
   this.$node.css('border-color', `rgb(${randomR}, ${randomG}, ${randomB})`);
 };
